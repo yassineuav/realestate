8d2b4c504fb6dcfc7670b5fb9210ed1ddbeecc1e
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2022-02-05 17:14:18 -0700

    Update Property.jsx

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,7 +5,7 @@ import { FaBed, FaBat, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-import DefaultImage from '../assets/images/House.jpg';
+import DefaultImage from '../../assets/images/House.jpg';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID } }) => (
 
