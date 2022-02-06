e0581377bd657c4ba90f90da3802aae4d67408d3
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2022-02-05 17:16:15 -0700

    Update Property.jsx

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,14 +5,14 @@ import { FaBed, FaBat, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-import DefaultImage from '../../assets/images/House.jpg';
+import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID } }) => (
 
     <Link href={`/property/${externalID}`} passHref >
         <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer" >
             <Box>
-                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="House" />
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house" />
             </Box>
             <Box w="full">
                 <Flex paddingTop="2" alignItems="center" justifyContent="space-between" >
